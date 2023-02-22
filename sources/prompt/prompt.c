873da9f2980feb401961161faa3f67e369926015
shelsonx <shelsonx@gmail.com>
2023-02-22 15:23:37 -0500

    move signal to up ft_strlen(line)

--- sources/prompt/prompt.c
+++ sources/prompt/prompt.c
@@ -59,17 +59,17 @@ void	run(t_parser *parser_data)
 		signal(SIGINT, sighandler);
 		make_prompt_text(parser_data);
 		parser_data->prompt->line = readline(parser_data->prompt->prompt_str);
+		if (parser_data->prompt->line == NULL)
+		{
+			free_signal(parser_data);
+			exit(EXIT_SUCCESS);
+		}
 		if (ft_strlen(parser_data->prompt->line) == 0)
 		{
 			free(parser_data->prompt->line);
 			free(parser_data->prompt->pwd);
 			free(parser_data->prompt->prompt_str);
 			continue ;
-		}
-		if (parser_data->prompt->line == NULL)
-		{
-			free_signal(parser_data);
-			exit(EXIT_SUCCESS);
 		}
 		add_history(parser_data->prompt->line);
 		init_parser(parser_data);
