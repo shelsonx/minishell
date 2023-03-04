45c23a8e00f9765013bb22c89eb106fca32de4c4
shelsonx <shelsonx@gmail.com>
2023-03-03 22:08:07 -0500

    solved extras tests

--- sources/exit_program/exit_program.c
+++ sources/exit_program/exit_program.c
@@ -20,12 +20,25 @@ void	error_command_msg(char **args, char *input_cmd)
 {
 	char	*msg;
 	char	*tmp;
+	int		fd;
 
 	if (args[0] == NULL && !is_dir_2(input_cmd) && (ft_strcmp(input_cmd, "") != 0))
 	{
 		tmp = ft_strjoin("minishell: ", input_cmd);
-		msg = ft_strjoin(tmp, ": command not found\n");
-		write(STDERR_FILENO, msg, ft_strlen(msg));
+		if (input_cmd[0] == '.' || input_cmd[0] == '/')
+		{
+			fd = open(input_cmd, O_RDONLY);
+			if (fd == -1)
+			{
+				perror("minishell: ");
+			}
+			msg = NULL;
+		}
+		else
+		{
+			msg = ft_strjoin(tmp, ": command not found\n");
+			write(STDERR_FILENO, msg, ft_strlen(msg));
+		}
 		free(tmp);
 		free(msg);
 	}
